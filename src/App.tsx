43ba8f97ef47c634dbcc21a247e4a7af92afb00c
twunkdean-cloud <twunkdean@gmail.com>
2025-10-22 03:20:06 -0400

    [dyad] Fixed AuthProvider context error - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -18,37 +18,40 @@ import ChecklistPage from "./pages/ChecklistPage";
 import TributeTrackerPage from "./pages/TributeTrackerPage"; // Import the new TributeTrackerPage
 import ChatAssistantPage from "./pages/ChatAssistantPage"; // Import the new ChatAssistantPage
 import NotFound from "./pages/NotFound";
+import { AuthProvider } from "./context/AuthContext";
 
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Layout> {/* Wrap all routes with the Layout component */}
-          <Routes>
-            <Route path="/" element={<DashboardPage />} />
-            <Route path="/twitter" element={<TwitterGeneratorPage />} />
-            <Route path="/reddit" element={<RedditGeneratorPage />} />
-            <Route path="/caption" element={<CaptionGeneratorPage />} />
-            <Route path="/image-vision" element={<ImageVisionPage />} />
-            <Route path="/responses" element={<ResponseTemplatesPage />} />
-            <Route path="/subs" element={<SubTrackerPage />} />
-            <Route path="/tributes" element={<TributeTrackerPage />} />
-            <Route path="/tasks" element={<TaskGeneratorPage />} />
-            <Route path="/pricing" element={<PricingPage />} />
-            <Route path="/settings" element={<SettingsPage />} />
-            <Route path="/checklist" element={<ChecklistPage />} />
-            <Route path="/chat-assistant" element={<ChatAssistantPage />} /> {/* New route */}
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Layout> {/* Wrap all routes with the Layout component */}
+            <Routes>
+              <Route path="/" element={<DashboardPage />} />
+              <Route path="/twitter" element={<TwitterGeneratorPage />} />
+              <Route path="/reddit" element={<RedditGeneratorPage />} />
+              <Route path="/caption" element={<CaptionGeneratorPage />} />
+              <Route path="/image-vision" element={<ImageVisionPage />} />
+              <Route path="/responses" element={<ResponseTemplatesPage />} />
+              <Route path="/subs" element={<SubTrackerPage />} />
+              <Route path="/tributes" element={<TributeTrackerPage />} />
+              <Route path="/tasks" element={<TaskGeneratorPage />} />
+              <Route path="/pricing" element={<PricingPage />} />
+              <Route path="/settings" element={<SettingsPage />} />
+              <Route path="/checklist" element={<ChecklistPage />} />
+              <Route path="/chat-assistant" element={<ChatAssistantPage />} /> {/* New route */}
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </AuthProvider>
 );
 
 export default App;
