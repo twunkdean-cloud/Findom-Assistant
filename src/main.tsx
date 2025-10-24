b6db74ca91c2677da752fd1ba6fa3c75d446e125
twunkdean-cloud <twunkdean@gmail.com>
2025-10-24 00:50:18 -0400

    [dyad] Fixed CSS import path error in main.tsx - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './globals.css';
 
 // Register service worker for offline support
 if ('serviceWorker' in navigator) {
